776f647c12a7d11d3090c0af8338734f63bed88f
Bailey Mejia <baileymejia28@gmail.com>
2024-08-13 21:04:31 -0700

    Added a User model to help create resolvers so I can query and create users, right now I am trying to fix a error to run the server.

--- src/app/Schemas/resolvers.ts
+++ src/app/Schemas/resolvers.ts
@@ -1,28 +1,44 @@
+import User  from '../models/userProfile';
+
+type MeArgs = {
+    username: string;
+}
+
+type Context = {
+    req: any;
+    res: any;
+    user?: any;
+}
+
+
 const resolvers = {
     Query: {
-        me: async(args, context) => {
-            //NEEED TO RETURN THE USER MODEL AND USE FINDONE METHOD
+        me: async (_: any, { username }: MeArgs, context: Context ) => {
+            const user = await User.findOne({ username} )
+            return user;
         }
     },
 
-    Mutation: {
-        signUp: async(args, context) => {
-            //NEED TO CREATE A NEW USER AND RETURN IT
-        },
-        login: async(args, context) => {
-            //NEED TO RETURN THE USER MODEL AND USE FINDONE METHOD
-        },
-        sendMessage: async(args, context) => {
-            //NEED TO CREATE A NEW MESSAGE AND RETURN IT
-        },
-        sendFriendRequest: async(args, context) => {
-            //NEED TO CREATE A NEW FRIEND REQUEST AND RETURN IT
-        },
-        acceptFriendRequest: async(args, context) => {
-            //NEED TO UPDATE THE FRIEND REQUEST STATUS TO ACCEPTED AND RETURN IT
-        },
-        declineFriendRequest: async(args, context) => {
-            //NEED TO UPDATE THE FRIEND REQUEST STATUS TO DECLINED AND RETURN IT
-        }
-    },
+    // Mutation: {
+    //     signUp: async(args, context) => {
+    //         //NEED TO CREATE A NEW USER AND RETURN IT
+    //     },
+    //     login: async(args, context) => {
+    //         //NEED TO RETURN THE USER MODEL AND USE FINDONE METHOD
+    //     },
+    //     sendMessage: async(args, context) => {
+    //         //NEED TO CREATE A NEW MESSAGE AND RETURN IT
+    //     },
+    //     sendFriendRequest: async(args, context) => {
+    //         //NEED TO CREATE A NEW FRIEND REQUEST AND RETURN IT
+    //     },
+    //     acceptFriendRequest: async(args, context) => {
+    //         //NEED TO UPDATE THE FRIEND REQUEST STATUS TO ACCEPTED AND RETURN IT
+    //     },
+    //     declineFriendRequest: async(args, context) => {
+    //         //NEED TO UPDATE THE FRIEND REQUEST STATUS TO DECLINED AND RETURN IT
+    //     }
+    // },
 };
+
+module.exports = resolvers;
---
+++ src/app/models/userProfile.ts
@@ -0,0 +1,32 @@
+import { Schema, model, connect } from "mongoose";
+import bcrypt from "bcryptjs";
+
+
+interface UserProfile {
+    email: string;
+    password: string;
+    username: string;
+}
+
+//ADD REFERENCES TO OTHER SCHEMAS IN THE FUTURE LIKE FRIENDREQUESTS, MESSAGES, ETC.
+const userSchema = new Schema<UserProfile>({
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    username: { type: String, required: true, unique: true }
+});
+
+const saltRounds = 10;
+
+userSchema.pre("save", async function (next) {
+    const user = this;
+    if (user.isModified("password")) {
+        user.password = await bcrypt.hash(user.password, saltRounds);
+    }
+    if (!user) {
+        throw new Error("User not found");
+    }   
+    next();
+});
+
+const User = model<UserProfile>("User", userSchema);
+export default User;
